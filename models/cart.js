73cf34a9d1b702a5616fa5837b867c875e13ecae
Jared Vandeventer <vanthedev@gmail.com>
2019-10-29 15:25:31 -0600

    mvc online store saved to files prior to database connection

---
+++ models/cart.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'cart.json'
+)
+
+module.exports = class Cart {
+  static addProduct(id, productPrice) {
+    fs.readFile(p, (err, data) => {
+      let cart = { products: [], totalPrice: 0 };
+      if (!err) {
+        cart = JSON.parse(data);
+      }
+      let updatedProduct;
+      const existingProductIndex = cart.products.findIndex(p => p.id === id);
+      const existingProduct = cart.products[existingProductIndex];
+      if (existingProduct) {
+        updatedProduct = {...existingProduct};
+        updatedProduct.qty = updatedProduct.qty + 1;
+        cart.products = [...cart.products];
+        cart.products[existingProductIndex] = updatedProduct;
+      } else {
+        updatedProduct = { id: id, qty: 1 };
+        cart.products = [...cart.products, updatedProduct];
+      }
+      cart.totalPrice = cart.totalPrice + +productPrice; 
+      fs.writeFile(p, JSON.stringify(cart), err => {
+        console.log(err);
+      })     
+    })
+  }
+
+  static deleteProduct = (id, productPrice) => {
+    fs.readFile(p, (err, data) => {
+      if (err) {
+        return;
+      } 
+      const updatedCart = { ...JSON.parse(data) };
+      const product = updatedCart.products.find(p => p.id === id);
+      if (!product) {
+        return;
+      }
+      const productQty = product.qty;
+      updatedCart.products = updatedCart.products.filter(
+        p => p.id !== id);
+        updatedCart.totalPrice = 
+        updatedCart.totalPrice - productPrice * productQty;
+      fs.writeFile(p, JSON.stringify(updatedCart), err => {
+        console.log(err);
+      })
+    })
+  }
+
+  static getProducts = (cb) => {
+    fs.readFile(p, (err, data) => {
+      const cart = JSON.parse(data);
+      if (err) {
+        cb(null);
+      } else {
+        cb(cart);
+      }
+    })
+  }
+}
